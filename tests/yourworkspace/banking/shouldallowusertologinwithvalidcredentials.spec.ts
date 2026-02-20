25b55dcbe6044e08867c33f50b409ed253655cc8
SakshiAphale <110682971+SakshiAphale@users.noreply.github.com>
2026-02-20 21:42:23 +0530

    test: add 2 test cases
    - should show error for invalid password (Tmlv35xut1)
    - should allow user to login with valid credentials (Tmlv35xus0)

---
+++ tests/yourworkspace/banking/shouldallowusertologinwithvalidcredentials.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+/**
+ * Test: should allow user to login with valid credentials
+ * Suite: Your Workspace > Banking > View Banking Dashboard
+ * Type: E2E
+ * Priority: P2
+ * ID: Tmlv35xus0
+ * 
+ * No description provided
+ */
+
+test.describe('Your Workspace > Banking > View Banking Dashboard', () => {
+  test('should allow user to login with valid credentials', async ({ page }) => {
+    // TODO: Implement test steps
+
+    // No steps defined - implement test logic here
+  });
+});
---
+++ tests/yourworkspace/banking/shouldshowerrorforinvalidpassword.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+/**
+ * Test: should show error for invalid password
+ * Suite: Your Workspace > Banking > View Banking Dashboard
+ * Type: E2E
+ * Priority: P2
+ * ID: Tmlv35xut1
+ * 
+ * No description provided
+ */
+
+test.describe('Your Workspace > Banking > View Banking Dashboard', () => {
+  test('should show error for invalid password', async ({ page }) => {
+    // TODO: Implement test steps
+
+    // No steps defined - implement test logic here
+  });
+});
